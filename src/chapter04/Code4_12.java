c9c0b36b875d46ab50a46eda85d54b5e15493ca6
hazyala <heamin0603@naver.com>
2025-04-14 11:49:45 +0900

    4장 데이터 자료형 1

---
+++ src/chapter04/Code4_12.java
@@ -0,0 +1,22 @@
+package chapter04;
+
+import java.util.Scanner;
+
+public class Code4_12 {
+    public static void main(String[] args) {
+       String str1 = "한국폴리텍대학 AI Software Seoul Jungsu";
+
+       String s1 = str1.toUpperCase();
+       System.out.println(s1);
+
+       String s2 = str1.toLowerCase();
+       System.out.println(s2);
+
+       System.out.println(str1);
+
+       String str2 = "     AI Software     Seoul     ";
+       System.out.println(str2);
+       String str3 = str2.trim();
+       System.out.println(str3);
+    }
+}
