6a70f52122df193c85fd8cab7ee9f4b4925cd834
hazyala <heamin0603@naver.com>
2025-06-16 11:51:59 +0900

    8장 상속1

---
+++ src/chapter08/section02/RabbitTest3.java
@@ -0,0 +1,11 @@
+package chapter08.section02;
+
+public class RabbitTest3 {
+    public static void main(String[] args) {
+        HouseRabbit h = new HouseRabbit();
+        h.move(300, 500);
+
+        MoutainRabbit m = new MoutainRabbit();
+        m.move(300, 500);
+    }
+}
